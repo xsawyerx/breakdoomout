cecff7871302b9470ea8d0f0781025df5395fac2
Sawyer X <xsawyerx@cpan.org>
2026-04-16 19:14:02 +0200

    stub ws endpoint at /doom

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require('http');
+const { WebSocketServer } = require('ws');
 const path = require('path');
 
 const app = express();
@@ -6,6 +8,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server, path: '/doom' });
+
+wss.on('connection', (ws) => {
+  console.log('[doom] client connected');
+  ws.on('close', () => console.log('[doom] client disconnected'));
+});
+
+server.listen(PORT, () => {
   console.log(`breakdoomout dev server on http://localhost:${PORT}`);
 });
